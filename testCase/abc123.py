0e2ae950c1bb879cb34fa75786f46c8c0296ffd1
qiujingxiong <908033789@qq.com>
2022-09-28 17:47:35 +0800

    新增可选择指定的table和sheet执行测试用例

--- testCase/abc123.py
+++
@@ -1,5 +0,0 @@
-from utils.utilsLoadYaml import yamlOptions
-
-
-# production_url = yamlOptions(yaml_file='config/requestDefault.yaml').read_yaml('server')
-# print(production_url)
--- testCase/apiKeys.py
+++ testCase/apiKeys.py
@@ -1,10 +1,10 @@
 import time
 import hashlib
 
-t = str(int(time.time())) # 获取10位时间戳
-rand = '0' # 随机值
-appId = '00015412_APP01' # appId
-appSecret = 'rvdkc20kimy5w1jgt9wqhjmpteokfq' # appSecret 
+t = str(int(time.time()))  # 获取10位时间戳
+rand = '0'  # 随机值
+appId = '00015412_APP01'  # appId
+appSecret = 'rvdkc20kimy5w1jgt9wqhjmpteokfq'  # appSecret
 # 拼接数据做md5加密
 auth_key1 = t + '-' + rand + '-' + appId + '-' + appSecret
 auth_key_md5 = hashlib.md5(auth_key1.encode('utf-8'))
