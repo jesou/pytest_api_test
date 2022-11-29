1b8fb9d186b52c281bda4fc2c4702265ebd8565d
qiujingxiong <908033789@qq.com>
2022-11-29 19:16:00 +0800

    指定修改测试

--- testCase/conftest.py
+++ testCase/conftest.py
@@ -33,44 +33,6 @@ def envs(request):
 # 读取yaml配置文件内容
 @pytest.fixture(scope="session", autouse=True)
 def get_env_config(envs='UAT'):
-    # if envs == 'dev':
-    #     yaml_dev_option = yamlOptions('config/test/configData.yaml')
-    #     dev_request_option = yamlOptions('config/test/requestDefault.yaml')
-    #     dev_db = yaml_dev_option.read_yaml('database')['sdc_dev']
-    #     dev_db_host = dev_db['aliyun_host']
-    #     dev_db_port = dev_db['aliyun_port']
-    #     dev_proxy_host = dev_db['proxy_host']
-    #     dev_proxy_port = dev_db['proxy_port']
-    #     dev_database = dev_db['database']
-    #     dev_username = dev_db['username']
-    #     dev_password = dev_db['password']
-    #     dev_auth_login = yaml_dev_option.read_yaml('auth_login')['appId']
-    #     dev_appId = dev_auth_login['appId']
-    #     dev_appSecret = dev_auth_login['appSecret']
-    #     dev_server = dev_request_option.read_yaml('server')
-    #     dev_url = dev_server['url']
-    #     return dev_db_host, dev_db_port, dev_proxy_host, dev_proxy_port, dev_database, dev_username, dev_password, \
-    #            dev_appId, dev_appSecret, dev_url
-    # elif envs == 'uat':
-    #     yaml_uat_option = yamlOptions('config/uat/configData.yaml')
-    #     uat_request_option = yamlOptions('config/uat/requestDefault.yaml')
-    #     uat_db = yaml_uat_option.read_yaml('database')['sdc_uat']
-    #     uat_db_host = uat_db['aliyun_host']
-    #     uat_db_port = uat_db['aliyun_port']
-    #     uat_proxy_host = uat_db['proxy_host']
-    #     uat_proxy_port = uat_db['proxy_port']
-    #     uat_database = uat_db['database']
-    #     uat_username = uat_db['username']
-    #     uat_password = uat_db['password']
-    #     uat_auth_login = yaml_uat_option.read_yaml('auth_login')['appId']
-    #     uat_appId = uat_auth_login['appId']
-    #     uat_appSecret = uat_auth_login['appSecret']
-    #     uat_server = uat_request_option.read_yaml('server')
-    #     uat_url = uat_server['url']['uat_url']
-    #     return uat_db_host, uat_db_port, uat_proxy_host, uat_proxy_port, uat_database, uat_username, uat_password, \
-    #            uat_appId, uat_appSecret, uat_url
-    # else:
-    #     return ValueError
     params_config = Config(envs)
     # db_host = params_config.database_host
     # db_port = params_config.database_port
--- utils/utilsRequests.py
+++ utils/utilsRequests.py
@@ -15,24 +15,22 @@ class RequestConfig:
         :param env: 选择执行环境
         """
         self.logger = logRecord().get_logger
-        self.pattern = r'{[A-Za-z0-9|/]+}'
         self.config = Config(env)
+        self.pattern = r'{[A-Za-z0-9|/]+}'
         try:
             self.url = self.config.url
             self.user = self.config.auth_appId
             self.password = self.config.auth_appSecret
-            print(self.user, self.password)
             self.uat_header = self.config.request_header
         except Exception as e:
             self.logger.exception('获取用例基本信息失败,{}'.format(e))
 
     # auth使用未完成
-    # def _request_auth(self, url: Text, header, params=None, method="get", use_auth=True):
-    #     if use_auth:
-    #         response_data = self._request_method(url=url, headers=header, data=params, method=method, auth=HTTPBasicAuth(self.user, self.password))
-    #     else:
-    #         response_data = self._request_method(url=url, header=header, data=params, method=method)
-    #     return response_data
+    @property
+    def _request_auth(self):
+        response_data = HTTPBasicAuth(self.user, self.password) if self.user is not None or \
+                                                                    self.password is not None else None
+        return response_data
 
     # @staticmethod
     # def get_auth_type(user, password, auth_type = None):
@@ -53,7 +51,6 @@ class RequestConfig:
             elif params is not None or params != '':
                 url = re.sub(self.pattern, params, url, count=1)
             params = ''
-
         self.logger.info('请求地址：%r, 请求方法：%s, 请求头：%s, 请求参数：%s'
                          % (url, method.lower(), header, params))
         # auth_type = get_auth_type(self.user, self.password) if (self.user is not None or self.password is not
@@ -62,33 +59,39 @@ class RequestConfig:
             if '{' not in params:
                 if params != '' or params is not None:
                     url = url + params
-                response_data = requests.get(url=url, headers=header, auth=HTTPBasicAuth(self.user, self.password))
+                response_data = requests.get(url=url, headers=header, auth=HTTPBasicAuth(self.user, self.password) if
+                                             self.user is not None or self.password is not None else None)
                 self.logger.info('响应码: %r, 响应结果: %s' % (response_data.status_code, response_data.text))
                 allureReportConfig.step("响应内容", "responseBody", response_data.text)
                 return response_data
             else:
-                response_data = requests.get(url=url, headers=header, params=params, auth=HTTPBasicAuth(self.user,
-                                                                                                        self.password))
+                response_data = requests.get(url=url, headers=header, params=params, auth=
+                                             HTTPBasicAuth(self.user, self.password) if self.user is not None or
+                                             self.password is not None else None)
                 self.logger.info('响应码: %r, 响应结果: %s' % (response_data.status_code, response_data.text))
                 allureReportConfig.step("响应内容", "responseBody", response_data.text)
                 return response_data
         elif method.upper() == 'POST':
             if postType == 'params':
+                # url 拼params
                 url = url + str(params)
-                response_data = requests.post(url=url, headers=header, auth=HTTPBasicAuth(self.user, self.password))
+                response_data = requests.post(url=url, headers=header, auth=HTTPBasicAuth(self.user, self.password) if
+                                              self.user is not None or self.password is not None else None)
                 self.logger.info('响应码: %r, 响应结果: %s' % (response_data.status_code, response_data.text))
                 allureReportConfig.step("响应内容", "responseBody", response_data.text)
                 return response_data
             else:
                 response_data = requests.post(url=url, headers=header, data=params,
-                                              auth=HTTPBasicAuth(self.user, self.password))
+                                              auth=HTTPBasicAuth(self.user, self.password) if self.user is not None or
+                                              self.password is not None else None)
                 self.logger.info('响应码: %r, 响应结果: %s' % (response_data.status_code, response_data.text))
                 allureReportConfig.step("响应内容", "responseBody", response_data.text)
             return response_data
         else:
             try:
                 response_data = requests.request(method.lower(), url=url, headers=header,
-                                                 data=params, auth=HTTPBasicAuth(self.user, self.password))
+                                                 data=params, auth=HTTPBasicAuth(self.user, self.password) if self.user
+                                                 is not None or self.password is not None else None)
                 self.logger.info('响应码: %r' % response_data.status_code)
                 allureReportConfig.step("响应内容", "responseBody", response_data.text)
                 return response_data
@@ -98,11 +101,11 @@ class RequestConfig:
 
 if __name__ == '__main__':
     RequestConfig()
-    # response = RequestConfig(). \
-    #     request_sampler(f'{Config().url}/v1/routes/routing/nodes',
-    #                     params='{"startPortCode":"CNWGQ","startPoint":{"lat":31.353633,"lon":121.617967},'
-    #                            '"endPortCode":"CNHUA", '
-    #                            '"endPoint":{"lat":38.316667,"lon":117.866667},"excludeNodes":[],"excludeSeaAreas":[],'
-    #                            '"withECA":true} ',
-    #                     method='post'
-    #                     )
+    response = RequestConfig(). \
+        request_sampler(f'{Config().url}/v1/routes/routing/nodes',
+                        params='{"startPortCode":"CNWGQ","startPoint":{"lat":31.353633,"lon":121.617967},'
+                               '"endPortCode":"CNHUA", '
+                               '"endPoint":{"lat":38.316667,"lon":117.866667},"excludeNodes":[],"excludeSeaAreas":[],'
+                               '"withECA":true} ',
+                        method='post'
+                        )
